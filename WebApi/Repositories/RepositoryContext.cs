2b36bf8e1cb873f6f8692d32a9eaeed42317e9df
Oğuzhan Denizhan <oguzhandenizhan25@gmail.com>
2025-06-07 23:25:35 +0300

    Section 6 Ef Core

---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+using WebApi.Repostories.Config;
+
+namespace WebApi.Repostories
+{
+    public class RepositoryContext:DbContext
+    {
+        public RepositoryContext(DbContextOptions options): base(options) 
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
+    }
+}
